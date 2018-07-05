79887c06f153c87f1c93c7b95c9b123aadf48832
crownclowngx <920432643@qq.com>
2018-07-05 23:38:53 +0800

    修改配置并创建项目

---
+++ TecheartSolution/TecheartSln.Plug.Classroom/Domain/QuestionSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecheartSln.Plug.Classroom.Domain
+{
+    public class QuestionSet
+    {
+    }
+
+    public class Question
+    {
+
+    }
+}
---
+++ TecheartSolution/TecheartSln.Plug.Classroom/ViewModel/SimpleExaminationViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Input;
+using TecheartSln.Core.Command;
+using TecheartSln.Core.Message.DeliverProvider;
+using TecheartSln.Core.Message.MessageTypeProvider;
+using TecheartSln.Core.Message.RelationProvider;
+using TecheartSln.Core.Scene;
+using TecheartSln.Core.ViewModel.Base;
+
+namespace TecheartSln.Plug.Classroom.ViewModel
+{
+    public class SimpleExaminationViewModel : TemplateBaseViewModel
+    {
+        public SimpleExaminationViewModel(string title) : base(title)
+        {
+        }
+
+        public SimpleExaminationViewModel(BaseScene scene, String json) : base(scene, json)
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            
+        }
+
+
+
+        RelayCommand _closeCommand = null;
+        override public ICommand CloseCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                {
+                    _closeCommand = new RelayCommand((p) => OnClose(), (p) => true);
+                }
+
+                return _closeCommand;
+            }
+        }
+
+        private void OnClose()
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            MessageDeliverGroup.Delivery(MessageType.CloseTempleteEvemt, javaScriptSerializer.Serialize(new CloseTempleteRequest() { TempleteGuid = this.StrGuid }));
+            MessageSubscribeRelations.DeleteSubscribe(MessageType.WSDEDataEvent, this.StrGuid);
+        }
+
+        public override ICommand SaveCommand => throw new NotImplementedException();
+    }
+}
