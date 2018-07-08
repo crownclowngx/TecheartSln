74f983114c0040f43a4eaf0fec5d51354d20b8b5
crownclowngx <920432643@qq.com>
2018-07-08 23:20:40 +0800

    增加excel操作的公共包

--- TecheartSolution/Test/Program.cs
+++ TecheartSolution/Test/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Aspose.Cells;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,9 +17,20 @@ namespace Test
             {
 
                 Console.WriteLine(Guid.NewGuid().ToString());
-                dynamic dy = new { kkk = 1 };
-                var s = dy.kkk;
             }
+
+            Workbook BuildReport_WorkBook = new Workbook();
+            Worksheet BuildReport_WorkSheet = BuildReport_WorkBook.Worksheets[0];
+            BuildReport_WorkSheet.FreezePanes(1, 1, 1, 0); //冻结第一行
+            Cells BuildReportCells = BuildReport_WorkSheet.Cells;
+            BuildReportCells[0, 0].PutValue("姓名");
+            BuildReportCells[0, 1].PutValue("Id");
+            BuildReportCells[0, 2].PutValue("出生年月");
+            BuildReportCells[0, 3].PutValue("年龄");
+            BuildReport_WorkSheet.Name = "users";
+            BuildReport_WorkSheet.AutoFitColumns();
+            BuildReport_WorkBook.Save("test.xlsx", SaveFormat.Xlsx);
+
             Console.Read();
         }
     }
