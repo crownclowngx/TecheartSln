7e716833fd839350685f6ca11b1060c16dfa2d9a
crownclowngx <920432643@qq.com>
2018-07-14 21:11:25 +0800

    修改 一个jsonUtils 线程安全问题

--- TecheartSolution/TecheartSln.Core/Utils/JsonUtils.cs
+++ TecheartSolution/TecheartSln.Core/Utils/JsonUtils.cs
@@ -9,20 +9,22 @@ namespace TecheartSln.Core.Utils
 {
     public static class JsonUtils
     {
-        static JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
 
         public static String Serialize<T>(T obj)
         {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
             return javaScriptSerializer.Serialize(obj);
         }
 
         public static String Serialize(object obj)
         {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
             return javaScriptSerializer.Serialize(obj);
         }
 
         public static T Deserialize<T>(String jsonstr)
         {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
             return javaScriptSerializer.Deserialize<T>(jsonstr);
         }
     }
